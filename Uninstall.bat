8149957d52a3b040b735afd0c93ab57347da0b7b
Dhen Ghiee <143119704+dhenghiee@users.noreply.github.com>
2024-09-13 03:07:49 +0700

    Add files via upload

---
+++ Uninstall.bat
@@ -0,0 +1,13 @@
+@echo off 
+title Uninstall Gnbots :-( 
+set arg1=%1 
+IF NOT "%arg1%" == "uninstall"( 
+    exit 
+) 
+set location =%cd% 
+IF NOT EXIST "%location%\GnBots.exe" ( 
+    exit 
+) 
+cd.. 
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GnBots /f \nrmdir /S /Q %location% 
+exit
